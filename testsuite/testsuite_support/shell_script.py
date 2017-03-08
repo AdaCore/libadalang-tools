c0ac0675ac1ccf6055f978d685b1e2cd3d9e41b0
Vasiliy Fofanov <fofanov@adacore.com>
2017-03-08 11:39:40 -0500

    Fix sh driver commit. PB21-019

--- testsuite/testsuite_support/shell_script.py
+++ testsuite/testsuite_support/shell_script.py
@@ -39,7 +39,7 @@ class ShellScriptDriver(BaseDriver):
     @catch_test_errors
     def run(self):
         os.chmod(os.path.join(self.working_dir(), 'test.sh'), 0755)
-        self.call_and_check(['test.sh'])
+        self.call_and_check(['sh', '-c', './test.sh'])
         if self.diff:
             for diff_pair in self.diff:
                 self.call(['diff', '-r'] + diff_pair.split() +
