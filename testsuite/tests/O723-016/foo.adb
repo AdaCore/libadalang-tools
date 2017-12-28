da59d8e52135da68e8933267e4dc1665dd5f78f1
Bob Duff <duff@adacore.com>
2017-12-28 15:41:35 -0500

    test for O723-016

---
+++ testsuite/tests/O723-016/foo.adb
@@ -0,0 +1,9 @@
+    with Toto,
+         Titi;
+
+    function Foo(Profondeur : in Natural ; b : natural) return Float is
+    a : constant := 100000000000000;
+    c: constant := 2#10101010101010101#;
+    begin
+        null;
+    end Foo;
---
+++ testsuite/tests/O723-016/test.sh
@@ -0,0 +1,1 @@
+gnatpp -q foo.adb -nM -neM -neD --no-separate-loop-then --no-separate-is --separate-stmt-name --use-on-new-line -M120 –decimal-grouping=3 –based-grouping=4 -pipe
