558cf490df82e07a5f83ae528c48ac73fa2d9f16
Bob Duff <duff@adacore.com>
2018-06-02 11:19:14 -0400

    lalpp: enable token mismatch in a test
    ...so I can see a stack traceback when it fails.
    Q518-056
    
    Change-Id: I83f4d3c866cd4768c628b1a1625a5ba023d7c558

--- testsuite/tests/Q320-051.pp.ca-z/test.sh
+++ testsuite/tests/Q320-051.pp.ca-z/test.sh
@@ -1,2 +1,2 @@
-gnatpp -q -P proj.gpr --output-dir=actual --name_mixed_case --based-grouping=4 --decimal-grouping=3 --comments-fill
+gnatpp -d5 -q -P proj.gpr --output-dir=actual --name_mixed_case --based-grouping=4 --decimal-grouping=3 --comments-fill
 diff -r expected actual | head -400
