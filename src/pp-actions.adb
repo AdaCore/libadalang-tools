7cf5901388700586662e130e52eb958c2b130b75
Raphaël AMIARD <raph.amiard@gmail.com>
2018-10-18 15:10:33 +0200

    RA10-018: Handle Ada_Discrete_Base_Subtype_Decl in Pp.Actions
    Change-Id: I8e51b28d77ad77633d49b075424659d9460e1fe8

--- src/pp-actions.adb
+++ src/pp-actions.adb
@@ -530,6 +530,7 @@ package body Pp.Actions is
    begin
       return
         (case Kind is
+           when Ada_Discrete_Base_Subtype_Decl => null,
            when Ada_Discrete_Subtype_Name => null,
            when Ada_Unconstrained_Array_Index => null,
            when Ada_Contract_Case_Assoc => null,
