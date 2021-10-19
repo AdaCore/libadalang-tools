12a7ced38e9f38c9f89e5c9f43639834164171d2
creuse <creuse@adacore.com>
2021-10-19 12:04:25 +0200

    Initial proposal of type representation
    no-tn-check
    
    Change-Id: I2c3ed68844214c253203cba756d01f912bcf4867

---
+++ src/tgen/tgen-types.adb
@@ -0,0 +1,75 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                  TGen                                    --
+--                                                                          --
+--                       Copyright (C) 2021, AdaCore                        --
+--                                                                          --
+-- TGen  is  free software; you can redistribute it and/or modify it  under --
+-- under  terms of  the  GNU General  Public License  as  published by  the --
+-- Free  Software  Foundation;  either version 3, or  (at your option)  any --
+-- later version. This software  is distributed in the hope that it will be --
+-- useful but  WITHOUT  ANY  WARRANTY; without even the implied warranty of --
+-- MERCHANTABILITY  or  FITNESS  FOR A PARTICULAR PURPOSE.                  --
+--                                                                          --
+-- As a special  exception  under  Section 7  of  GPL  version 3,  you are  --
+-- granted additional  permissions described in the  GCC  Runtime  Library  --
+-- Exception, version 3.1, as published by the Free Software Foundation.    --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and a  --
+-- copy of the GCC Runtime Library Exception along with this program;  see  --
+-- the files COPYING3 and COPYING.RUNTIME respectively.  If not, see        --
+-- <http://www.gnu.org/licenses/>.                                          --
+------------------------------------------------------------------------------
+
+with Langkit_Support.Text;
+
+package body TGen.Types is
+
+   package Text renames Langkit_Support.Text;
+
+   function Image (Self : Typ) return String is
+   begin
+      return Text.Image (Self.Name.Text);
+   end Image;
+
+   function Image (Self : Signed_Int_Typ) return String is
+   begin
+      return
+        (Typ (Self).Image & ": Signed Integer range"
+         & Self.Rang.Min'Image & " .." & Self.Rang.Max'Image);
+   end Image;
+
+   function Image (Self : Mod_Int_Typ) return String is
+   begin
+      return
+        (Typ (Self).Image & ": Modular Integer mod" & Self.Mod_Value'Image);
+   end Image;
+
+   function Image (Self : Float_Typ) return String is
+   begin
+      return
+        (Typ (Self).Image & ": Real Type digits" & Self.Precision'Image &
+         (if Self.Has_Range then
+            " range" & Self.Rang.Min'Image & " .." & Self.Rang.Max'Image
+          else ""));
+   end Image;
+
+   function Image (Self : Ordinary_Fixed_Typ) return String is
+   begin
+      return
+        (Typ (Self).Image & ": Ordinary Fixed Point delta" &
+         Self.Delta_Value'Image & " range" & Self.Rang.Min'Image & " .." &
+         Self.Rang.Max'Image);
+   end Image;
+
+   function Image (Self : Decimal_Fixed_Typ) return String is
+   begin
+      return
+        Typ (Self).Image & ": Decimal Fixed Point delta" &
+        Self.Delta_Value'Image & " digits" & Self.Digits_Value'Image &
+        (if Self.Has_Range then
+           " range" & Self.Rang.Min'Image & " .." & Self.Rang.Max'Image
+         else "");
+   end Image;
+
+end TGen.Types;
---
+++ src/tgen/tgen-types.ads
@@ -0,0 +1,127 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                  TGen                                    --
+--                                                                          --
+--                       Copyright (C) 2021, AdaCore                        --
+--                                                                          --
+-- TGen  is  free software; you can redistribute it and/or modify it  under --
+-- under  terms of  the  GNU General  Public License  as  published by  the --
+-- Free  Software  Foundation;  either version 3, or  (at your option)  any --
+-- later version. This software  is distributed in the hope that it will be --
+-- useful but  WITHOUT  ANY  WARRANTY; without even the implied warranty of --
+-- MERCHANTABILITY  or  FITNESS  FOR A PARTICULAR PURPOSE.                  --
+--                                                                          --
+-- As a special  exception  under  Section 7  of  GPL  version 3,  you are  --
+-- granted additional  permissions described in the  GCC  Runtime  Library  --
+-- Exception, version 3.1, as published by the Free Software Foundation.    --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and a  --
+-- copy of the GCC Runtime Library Exception along with this program;  see  --
+-- the files COPYING3 and COPYING.RUNTIME respectively.  If not, see        --
+-- <http://www.gnu.org/licenses/>.                                          --
+------------------------------------------------------------------------------
+
+with Libadalang.Analysis;
+
+with Ada.Unchecked_Deallocation;
+
+package TGen.Types is
+
+   package LAL renames Libadalang.Analysis;
+
+   type Int_Range is record
+      Min, Max : Integer;
+   end record;
+
+   type Float_Range is record
+      Min, Max : Long_Float;
+   end record;
+
+   type Typ is tagged record
+      Name : LAL.Defining_Name;
+   end record;
+
+   function Image (Self : Typ) return String;
+
+   type Scalar_Typ is new Typ with null record;
+
+   type Discrete_Typ is new Scalar_Typ with null record;
+
+   type Enum_Typ is new Discrete_Typ with null record;
+
+   type Enum_Lit is record
+
+      Pos : Natural;
+      Id  : LAL.Identifier;
+
+   end record;
+
+   type Enum_Lit_Arr is array (Natural range <>) of Enum_Lit;
+
+   type Enum_Lit_Acc is access Enum_Lit_Arr;
+
+   type Char_Typ is new Enum_Typ with null record;
+
+   type Bool_Typ is new Enum_Typ with null record;
+
+   type Other_Enum_Typ is new Enum_Typ with record
+      Litterals : Enum_Lit_Acc;
+   end record;
+
+   type Int_Typ is new Discrete_Typ with null record;
+
+   type Signed_Int_Typ is new Int_Typ with record
+      Rang : Int_Range;
+   end record;
+
+   function Image (Self : Signed_Int_Typ) return String;
+
+   type Mod_Int_Typ is new Int_Typ with record
+      Mod_Value : Integer;
+   end record;
+
+   function Image (Self : Mod_Int_Typ) return String;
+
+   type Real_Typ is new Scalar_Typ with null record;
+
+   type Float_Typ (Has_Range : Boolean) is new Real_Typ with record
+      Precision : Natural;
+      case Has_Range is
+         when True =>
+            Rang : Float_Range;
+         when False =>
+            null;
+      end case;
+   end record;
+
+   function Image (Self : Float_Typ) return String;
+
+   type Ordinary_Fixed_Typ is new Real_Typ with record
+      Delta_Value : Long_Float;
+      Rang        : Float_Range;
+   end record;
+
+   function Image (Self : Ordinary_Fixed_Typ) return String;
+
+   type Decimal_Fixed_Typ (Has_Range : Boolean) is new Real_Typ with record
+      Delta_Value  : Long_Float;
+      Digits_Value : Natural;
+      case Has_Range is
+         when True =>
+            Rang : Float_Range;
+         when others =>
+            null;
+      end case;
+   end record;
+
+   function Image (Self : Decimal_Fixed_Typ) return String;
+
+   type Access_Typ is new Typ with null record;
+
+   type Composite_Typ is new Typ with null record;
+
+   type Typ_Acc is access Typ'Class;
+
+   procedure Free is new Ada.Unchecked_Deallocation (Typ'Class, Typ_Acc);
+
+end TGen.Types;
