62315621614d04cbe98704f86ffd05e2fee73fd7
Monika Kurovszky <kurovszky@adacore.com>
2021-11-25 16:45:42 +0100

    U208-025: Adding indentation after split
      The Ada_Witch_Clause template is changed in order
      to be able to add indentation of the lines after
      spliting long withed packages list lines.
    
      The test case is updated.
    
    Change-Id: Ica05fd3d19042400f6730ab3e725e6d73b110ac9
    Depends-On: I90dcb515e16693064c3aae9bcd6088b134d33c4b

--- testsuite/tests/pp/U208-025/simple.adb
+++ testsuite/tests/pp/U208-025/simple.adb
@@ -1,5 +1,6 @@
 
 with Package1, Package2, Package3, Package4, Package5, Package6, Package6, Package7, Package8, Package9, Package10, Package11, Package12, Package13, Package14, Package15, Package16, Package17, Package18, Package19, Package20, Package21, Package22, Package23;
+use Package1, Package2, Package3, Package4, Package5, Package6, Package6, Package7, Package8, Package9, Package10, Package11, Package12, Package13, Package14, Package15, Package16, Package17, Package18, Package19, Package20, Package21, Package22, Package23;
 
 
 procedure Simple is
