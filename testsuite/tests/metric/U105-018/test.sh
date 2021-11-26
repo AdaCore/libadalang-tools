f25d0505b9c5a12d0dda622b0baa88bb8297bbbb
Monika Kurovszky <kurovszky@adacore.com>
2021-11-26 10:48:05 +0100

    U105-018: updating baseline to fix nightly regression
    Change-Id: I43945c9d9687d061b702fab36c682f858b49999d

--- testsuite/tests/metric/U105-018/test.sh
+++ testsuite/tests/metric/U105-018/test.sh
@@ -1,4 +1,4 @@
 rm -rf obj
 
-gnatmetric -ox metrix.xml -P simple.gpr -Xmode=gmfiles simple.adb
+gnatmetric -ox metrix.xml -P simple.gpr --short-file-names -Xmode=gmfiles simple.adb
 cat obj/gmfiles/simple.adb.metrix
