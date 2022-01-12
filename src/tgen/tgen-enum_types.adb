618519fca8c6c08f2228c642ce23e7d93360d79d
creuse <creuse@adacore.com>
2022-01-12 13:13:13 +0100

    Update copyright notice year
    no-tn-check
    
    Change-Id: I89c6f994bdcb38c77567ae3a3c23e10b9a0093c9

--- src/tgen/tgen-enum_types.adb
+++ src/tgen/tgen-enum_types.adb
@@ -2,7 +2,7 @@
 --                                                                          --
 --                                  TGen                                    --
 --                                                                          --
---                       Copyright (C) 2021, AdaCore                        --
+--                       Copyright (C) 2022, AdaCore                        --
 --                                                                          --
 -- TGen  is  free software; you can redistribute it and/or modify it  under --
 -- under  terms of  the  GNU General  Public License  as  published by  the --
