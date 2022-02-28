3128d5856f3c56a6ca739ea677ce9a839d00e4f2
Matthieu Eyraud <eyraud@adacore.com>
2022-02-28 15:13:41 +0100

    Fix build
    Change-Id: Ib14912290d60b47526b6ac47f12299187802bb61

--- src/tgen/tgen-types-array_types.ads
+++ src/tgen/tgen-types-array_types.ads
@@ -27,8 +27,8 @@ with TGen.Types.Constraints; use TGen.Types.Constraints;
 
 package TGen.Types.Array_Types is
 
-   Unconstrained_Array_Size_Min : constant Positive := 0;
-   Unconstrained_Array_Size_Max : constant Positive := 10;
+   Unconstrained_Array_Size_Min : constant Natural := 0;
+   Unconstrained_Array_Size_Max : constant Natural := 10;
    --  Min / max size of generated unconstrained arrays. Hardcoded at the
    --  moment.
 
