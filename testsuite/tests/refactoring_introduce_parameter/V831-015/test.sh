3ac3713cd1e6feeb721174d3df2a9b67552cc765
Joao Azevedo <azevedo@adacore.com>
2022-09-07 10:58:35 +0100

    V823-008 V831-015 Fix pull up of object that depends on parameters
    Do not pull up dependencies that are parameters.
    Change the insertion point to above procedures body instead of spec.
    Update tests baseline accordingly.
    
    Change-Id: Id00e047bca0cd105f34552ab3e3ca86d0c367189

---
+++ testsuite/tests/refactoring_introduce_parameter/V831-015/test.sh
@@ -0,0 +1,1 @@
+introduce_parameter -P default.gpr --source main.adb --start-line 8 --start-column 10 --end-line 8 --end-column 11
---
+++ testsuite/tests/refactoring_pull_up_declaration/V823-008/pull_up_obj_decl_with_param_dep/main.adb
@@ -0,0 +1,15 @@
+procedure Main is
+begin
+   declare
+      Z : constant Integer := 10;
+      procedure Foo (X : Integer := Z);
+      procedure Foo (X : Integer := Z)
+      is
+         Y : Integer := X;
+      begin
+         null;
+      end Foo;
+   begin
+      Foo (Z);
+   end;
+end Main;
