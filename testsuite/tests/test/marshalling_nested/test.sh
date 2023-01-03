0c382dcb80da21ded63d4050dcf09f749f8259a0
Léo Creuse <creuse@adacore.com>
2023-01-03 12:56:18 +0100

    TGen: Fix marshalling tests on windows
    The test.sh scripts use [[ as the conditional evaluator, which is
    not portable, and apparently not present on the windows testing
    environment.
    
    This change modifies this by using a single [ instead to perform
    conditional checks.
    
    TN: VC07-013

--- testsuite/tests/test/marshalling_nested/test.sh
+++ testsuite/tests/test/marshalling_nested/test.sh
@@ -3,7 +3,7 @@
 LALTOOLS_ROOT=$(dirname $(which gnattest))/..
 TEMPLATES_PATH=$LALTOOLS_ROOT/share/tgen/templates
 RTS_PATH=$LALTOOLS_ROOT/share/tgen/tgen_rts
-if [[ "$OS" == "Windows_NT" ]]; then
+if [ "$OS" == "Windows_NT" ]; then
     export GPR_PROJECT_PATH=$RTS_PATH";"$GPR_PROJECT_PATH
 else
     export GPR_PROJECT_PATH=$RTS_PATH:$GPR_PROJECT_PATH
