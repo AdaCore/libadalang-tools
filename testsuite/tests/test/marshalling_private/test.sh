9a182ab2ed460c2a6985dced8ba0b06a4fe23f2f
Léo Creuse <creuse@adacore.com>
2023-03-03 16:02:30 +0100

    TGen: automatically install TGen_RTS in the testsuite working dir
    As test value generation now also relies on TGen_RTS, it is better
    to build and install it once in the temporary work dir of the
    testsuite, and let all the tests use that version.
    
    As part of V315-014

--- testsuite/tests/test/marshalling_private/test.sh
+++ testsuite/tests/test/marshalling_private/test.sh
@@ -2,12 +2,6 @@
 
 LALTOOLS_ROOT=$(dirname $(which gnattest))/..
 TEMPLATES_PATH=$LALTOOLS_ROOT/share/tgen/templates
-RTS_PATH=$LALTOOLS_ROOT/share/tgen/tgen_rts
-if [ "$OS" == "Windows_NT" ]; then
-    export GPR_PROJECT_PATH=$RTS_PATH";"$GPR_PROJECT_PATH
-else
-    export GPR_PROJECT_PATH=$RTS_PATH:$GPR_PROJECT_PATH
-fi
 mkdir -p test/obj obj
 tgen_marshalling -P test/test.gpr --templates-dir=$TEMPLATES_PATH -o test/tgen_support test/foo.ads
 gprbuild -q -P test_gen.gpr
--- testsuite/testsuite.py
+++ testsuite/testsuite.py
@@ -11,6 +11,9 @@ Run the libadalang-tools testsuite.
 import os
 
 import e3.testsuite
+from e3.fs import sync_tree
+from e3.os.fs import which
+from e3.os.process import Run
 
 from drivers.python_script import PythonScriptDriver
 from drivers.shell_script import ShellScriptDriver
@@ -43,6 +46,11 @@ class Testsuite(e3.testsuite.Testsuite):
             action="store_true",
             help="Rewrite test baselines according to current output.",
         )
+        parser.add_argument(
+            "--tgen-rts-dir",
+            action="store",
+            help="alternative path to the source of the TGen_RTS project"
+        )
 
     def set_up(self):
         super().set_up()
@@ -74,6 +82,46 @@ class Testsuite(e3.testsuite.Testsuite):
 
         # Turn on strict mode for gnattest to catch real errors
         os.environ["GNATTEST_STRICT"] = "TRUE"
+
+        # Copy, build, install and setup the TGen_RTS
+
+        # First, locate the rts sources and copy them in the working dir
+        if self.main.args.tgen_rts_dir:
+            if os.path.isabs (self.main.args.tgen_rts_dir):
+                rts_src_dir = self.main.args.tgen_rts_dir
+            else:
+                rts_src_dir = os.path.join(
+                    os.getcwd(),
+                    self.main.args.tgen_rts_dir
+                )
+        else:
+            rts_src_dir = os.path.join(
+                os.path.dirname(which("gnattest")),
+                "..",
+                "share",
+                "tgen",
+                "tgen_rts"
+            )
+        rts_working_dir = os.path.join(self.env.working_dir, "tgen_rts_src")
+        sync_tree(rts_src_dir, rts_working_dir)
+
+        # Build and install the RTS,
+        p = Run (["gprbuild", "-Ptgen_rts.gpr"], cwd=rts_working_dir)
+        if p.status == 0:
+            p = Run(
+                ["gprinstall",
+                 "-Ptgen_rts.gpr",
+                 "-p",
+                 "--prefix=local"],
+                 cwd=rts_working_dir
+            )
+
+        # Make it available through the GPR_PROJECT_PATH env var
+        if p.status == 0:
+            new_GPP = os.path.join(rts_working_dir, "local", "share", "gpr")
+            if "GPR_PROJECT_PATH" in os.environ:
+                new_GPP += os.pathsep + os.environ["GPR_PROJECT_PATH"]
+            os.environ["GPR_PROJECT_PATH"] = new_GPP
 
 
 if __name__ == "__main__":
