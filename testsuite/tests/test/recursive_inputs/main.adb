909b3f33685f62f5de414a542a8f0c3d63888681
Fedor Rybin <frybin@adacore.com>
2023-07-06 22:04:53 +0300

    Filter intermediate recursive inputs
    Rewrite user code instrumentation, use wrappers instead of injections.
    Add counter or recursion depth and only dump inputs of the first
    call in case of self-recursion.
    
    issue eng/ide/libadalang-tools#81

---
+++ testsuite/tests/test/recursive_inputs/main.adb
@@ -0,0 +1,7 @@
+with P;
+with Ada.Text_IO;
+procedure Main is
+begin
+   --  Ada.Text_IO.Put_Line (P.Fibonacci (5)'Img);
+   Ada.Text_IO.Put_Line (P.Factorial (5)'Img);
+end Main;
---
+++ testsuite/tests/test/recursive_inputs/p.ads
@@ -0,0 +1,9 @@
+package P is
+
+   function Fibonacci (Number : Integer) return Natural;
+   --  Has excpetion handling inside
+
+   function Factorial (Number : Integer) return Positive;
+   --  Does not have excpetion handling inside
+
+end P;
