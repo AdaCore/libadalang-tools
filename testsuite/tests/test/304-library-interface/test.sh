3c05f5cb0a06f00191384149b76822b490cee634
Léo Creuse <creuse@adacore.com>
2025-05-16 15:09:43 +0200

    Testsuite/tests/tes/304-library-interface: Simplify test output
    This makes the test less sensitive to changes in gprbuild's output
    depending on the platform.
    
    (cherry picked from commit 1127eb24848fdd25ebe014bcf96739338fd1de5c)

--- testsuite/tests/test/304-library-interface/test.sh
+++ testsuite/tests/test/304-library-interface/test.sh
@@ -2,4 +2,4 @@ gnattest -P prj_2.gpr --stub -q
 
 # We only check for correct compilation of the sources, if the interface is not
 # correctly specified, gprbuild will stop the compilation.
-make --no-print-directory -C obj_2/gnattest_stub/harness
+make --quiet -C obj_2/gnattest_stub/harness BUILDERFLAGS=-q
